c6c9e6544a76c75f913ea1c3d1b612ed0cdcd2ae
Marbel Arrieta <marbelarrietam@gmail.com>
2019-02-08 23:50:33 -0500

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
+  <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">TaskManager</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Authors</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <div class="container">
       <div class="row">
    <div class="col-sm">
@@ -85,5 +101,7 @@ export default {
 </script>
 
 <style>
-
+.container{
+  margin-top: 20px;
+}
 </style>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -2,13 +2,21 @@
   <div class="post__container">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title"></h5>
+        <h5 class="card-title">Task</h5>
         <p class="card-text"></p>
-        <p>{{description}}</p>
-        <p>{{author}}</p>
+        <p><span>Task: </span>{{description}}</p>
+        <p><span>Author: </span>{{author}}</p>
         <p>{{status}}</p>
-      </div>
-    </div>
+    <button type="button" class="btn btn-success" @click="save" >Complete task</button>
+    <button type="button" class="btn btn-success" @click="save" >Update</button>
+    <button type="button" class="btn btn btn-danger" @click="save" >Delete</button>
+    
+    <div v-if="status" class="progress">
+  <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+  </div>
+
+  </div>
   </div>
 </template>
 
@@ -30,3 +38,13 @@ export default {
   },
 };
 </script>
+
+<style>
+span {
+  font-weight:500;
+}
+
+.progress{
+  margin-top:15px;
+}
+</style>
